eaa3be51ff15e30a36d475ee988f918a5ef80272
Philipp Kastl <picastle@posteo.de>
2022-02-17 10:14:00 +0100

    create extra component for chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,20 @@
 import './App.sass';
 import React from 'react';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
 import { useFoodAPI } from './api';
+import { Chart } from './components/Chart';
 
 function App() {
   const initialValue = '';
 
   const [input, setInput] = React.useState(initialValue);
-  ChartJS.register(ArcElement, Tooltip, Legend);
 
   const { nutrients, fetchData, state } = useFoodAPI();
   console.log(nutrients);
-  const chartData = {
-    labels: ['fat', 'fiber', 'carbs', 'proteins'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [
-          [nutrients][0][0].fat,
-          [nutrients][0][1].fiber,
-          [nutrients][0][2].carbs,
-          [nutrients][0][3].proteins,
-        ],
-        backgroundColor: [
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-        ],
-        borderColor: [
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
 
   const handleChange = ({ target }) => {
     const inputValue = target.value;
     setInput({ ...input, inputValue });
   };
-
-  // React.useEffect(() => {
-  //   fetchData();
-  // }, []);
 
   return (
     <div className='App'>
@@ -106,9 +73,7 @@ function App() {
               );
             })}
         </div>
-        <div className='ctn-chart'>
-          <Pie data={chartData} />
-        </div>
+        <Chart nutrients={nutrients} />
       </div>
       <footer>&copy; Philipp Kastl, 2022</footer>
     </div>
---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,42 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function Chart({ nutrients }) {
+  console.log(nutrients);
+  const chartData = {
+    labels: ['fat', 'fiber', 'carbs', 'proteins'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [
+          [nutrients][0][0].fat,
+          [nutrients][0][1].fiber,
+          [nutrients][0][2].carbs,
+          [nutrients][0][3].proteins,
+        ],
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  return (
+    <div className='ctn-chart'>
+      <Pie data={chartData} />
+    </div>
+  );
+}
+
+export default Chart;
